7220be9f603cdbeeb546fd52a100f456d8b815c6
AarushGroup <aarushgroupin@gmail.com>
2023-05-12 10:41:21 +0530

    Created startups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartUpsComponent } from './start-ups/start-ups.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StartUpsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/start-ups/start-ups.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StartUpsComponent } from './start-ups.component';
+
+describe('StartUpsComponent', () => {
+  let component: StartUpsComponent;
+  let fixture: ComponentFixture<StartUpsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StartUpsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StartUpsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
