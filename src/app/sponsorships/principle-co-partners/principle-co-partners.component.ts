b9deac3732d4b9606d7883cc885e29f818e676b7
AarushGroup <aarushgroupin@gmail.com>
2023-05-15 14:10:38 +0530

    Fixed css issues

--- src/app/sponsorships/principle-co-partners/principle-co-partners.component.ts
+++ src/app/sponsorships/principle-co-partners/principle-co-partners.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from "aos";
 
 @Component({
   selector: 'app-principle-co-partners',
@@ -10,6 +11,7 @@ export class PrincipleCoPartnersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
   }
 
 }
