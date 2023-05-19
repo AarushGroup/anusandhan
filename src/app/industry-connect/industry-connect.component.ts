6961abbc8a4b761d45bc95c0bcdd996c8298e240
venup <venup.in@mouritech.com>
2023-05-19 16:35:42 +0530

    ui changes in Industry 4.0 section

--- src/app/industry-connect/industry-connect.component.ts
+++ src/app/industry-connect/industry-connect.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-industry-connect',
   templateUrl: './industry-connect.component.html',
-  styleUrls: ['./industry-connect.component.css']
+  styleUrls: ['./industry-connect.component.css'],
 })
 export class IndustryConnectComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    AOS.init();
   }
-
 }
